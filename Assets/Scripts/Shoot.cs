4746c5cde0c409c929a305c8fdb2d495bbcfb8ec
Bryan <dinsbachbryan@gmail.com>
2023-05-29 15:22:58 +0200

    Add script changes

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,24 +6,38 @@ public class Shoot : MonoBehaviour
     [SerializeField] private GameObject _bulletPrefab;
     [SerializeField] private float _fireRate;
     [SerializeField] private float _bulletSpeed;
-    
+    [SerializeField] private GameObject[] _smokeVFX;
 
     private float _nextFireTime;
     
     public void Shooting()
     {
+        if (!CanAttack())
+        {
+            return;
+        }
+
+        /*foreach (var smokeVFX in _smokeVFX)
+        {
+            smokeVFX.SetActive(true);
+        }*/
+        
+        foreach (var cannon in _bulletSpawnPoints)
+        {
+            GameObject bullet = Instantiate(_bulletPrefab, cannon.position, cannon.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = cannon.forward * _bulletSpeed;
+        }
+    }
+
+    public bool CanAttack()
+    {
         if (Time.time >= _nextFireTime)
         {
-            foreach (var cannon in _bulletSpawnPoints)
-            {
-                GameObject bullet = Instantiate(_bulletPrefab, cannon.position, cannon.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = cannon.forward * _bulletSpeed;
-                
-                _nextFireTime = Time.time + (1f / _fireRate);
-            }
+            _nextFireTime = Time.time + (1f / _fireRate);
+            return true;
         }
-        
-        
+
+        return false;
     }
 }
 
